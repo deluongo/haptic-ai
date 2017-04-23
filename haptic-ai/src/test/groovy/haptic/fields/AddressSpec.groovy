211721a3af85c672fce45f1f4085406c4664b53e
Sachin Srivastava <sachinsrivastava0126@gmail.com>
2017-04-23 19:21:49 -0400

    Address constraints tests.

--- haptic-ai/src/test/groovy/haptic/fields/AddressSpec.groovy
+++ haptic-ai/src/test/groovy/haptic/fields/AddressSpec.groovy
@@ -15,8 +15,110 @@ class AddressSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+    void "Test locName not blank"() {
+        when: "Location name is blank"
+            Address testAddress = new Address(  locName: '',
+                                                locType: 'Main Office',
+                                                streetNumber: 20,
+                                                streetName: 'Harvard Street',
+                                                city: 'Boston',
+                                                state: 'MA',
+                                                country: 'United States Of America',
+                                                zip: '12345')
+
+        then: "Fail validation"
+            !testAddress.validate()
     }
+
+    void "Test locType not blank"() {
+        when: "Location type is blank"
+            Address testAddress = new Address(  locName: 'Harvard University',
+                                                locType: '',
+                                                streetNumber: 20,
+                                                streetName: 'Harvard Street',
+                                                city: 'Boston',
+                                                state: 'MA',
+                                                country: 'United States Of America',
+                                                zip: '12345')
+
+        then: "Fail validation"
+            !testAddress.validate()
+    }
+
+    void "Test streetName not blank"() {
+        when: "Street name is blank"
+            Address testAddress = new Address(  locName: 'Harvard University',
+                                                locType: 'Main Office',
+                                                streetNumber: 20,
+                                                streetName: '',
+                                                city: 'Boston',
+                                                state: 'MA',
+                                                country: 'United States Of America',
+                                                zip: '12345')
+
+        then: "Fail validation"
+            !testAddress.validate()
+    }
+
+    void "Test city not blank"() {
+        when: "City is blank"
+            Address testAddress = new Address(  locName: 'Harvard University',
+                                                locType: 'Main Office',
+                                                streetNumber: 20,
+                                                streetName: 'Harvard Street',
+                                                city: '',
+                                                state: 'MA',
+                                                country: 'United States Of America',
+                                                zip: '12345')
+
+        then: "Fail validation"
+            !testAddress.validate()
+    }
+
+    void "Test state not blank"() {
+        when: "State is blank"
+            Address testAddress = new Address(  locName: 'Harvard University',
+                                                locType: 'Main Office',
+                                                streetNumber: 20,
+                                                streetName: 'Harvard Street',
+                                                city: 'Boston',
+                                                state: '',
+                                                country: 'United States Of America',
+                                                zip: '12345')
+
+        then: "Fail validation"
+            !testAddress.validate()
+    }
+
+    void "Test country not blank"() {
+        when: "Country is blank"
+            Address testAddress = new Address(  locName: 'Harvard University',
+                                                locType: 'Main Office',
+                                                streetNumber: 20,
+                                                streetName: 'Harvard Street',
+                                                city: 'Boston',
+                                                state: 'MA',
+                                                country: '',
+                                                zip: '12345')
+
+        then: "Fail validation"
+            !testAddress.validate()
+    }
+
+    void "Test zip code in proper format"() {
+        when: "Zip is not in proper format"
+            Address testAddress = new Address(  locName: 'Harvard University',
+                                                locType: 'Main Office',
+                                                streetNumber: 20,
+                                                streetName: 'Harvard Street',
+                                                city: 'Boston',
+                                                state: 'MA',
+                                                country: 'United States of America',
+                                                zip: '0')
+
+        then: "Fail validation"
+            !testAddress.validate()
+    }
+
+
 }
