ed228b88fdf37e3e67c938f44c84787d0c096d84
Sachin Srivastava <sachinsrivastava0126@gmail.com>
2017-05-02 00:13:23 -0400

    Bootstrap properly setting up objects.

--- haptic-ai/grails-app/init/haptic/ai/BootStrap.groovy
+++ haptic-ai/grails-app/init/haptic/ai/BootStrap.groovy
@@ -5,64 +5,70 @@ import haptic.app.User
 import haptic.app.Role
 import haptic.crm.Company
 import haptic.crm.Contact
-import haptic.fields.EmailAddress
 import haptic.crm.Lead
+import haptic.fields.EmailAddress
 
 class BootStrap {
 
     def init = { servletContext ->
-//        environments {
-//            development {
-//                //setupUsersAndRoles()
-//            }
-//            test {
-//                //setupUsersAndRoles()
-//            }
-//            production {
-//                // do nothing
-//            }
-//        }
+        environments {
+            development {
+                setupUsersAndRoles()
+                setupDummyData()
+            }
+            test {
+                //setupUsersAndRoles()
+                setupDummyData()
+            }
+            production {
+                // do nothing
+            }
+        }
+    }
+    def destroy = {
+    }
 
+    def setupDummyData() {
         // Emails
         HashSet<EmailAddress> billEmail = new HashSet<EmailAddress>()
         EmailAddress email1 = new EmailAddress(emailType: 'Primary', emailAddress: 'bill@microsoft.com', emailExtension: '@microsoft.com',
-                                                emailStatus: 'Active', dateCreated: new Date())
+                emailStatus: 'Active', dateCreated: new Date())
         email1.save(flush: true)
         billEmail.add(email1)
 
         HashSet<EmailAddress> steveEmail = new HashSet<EmailAddress>()
         EmailAddress email2 = new EmailAddress(emailType: 'Primary', emailAddress: 'steve@apple.com', emailExtension: '@apple.com',
-                                                emailStatus: 'Active', dateCreated: new Date())
+                emailStatus: 'Active', dateCreated: new Date())
         email2.save(flush: true)
         steveEmail.add(email2)
 
         HashSet<EmailAddress> bill2Email = new HashSet<EmailAddress>()
         EmailAddress email3 = new EmailAddress(emailType: 'Primary', emailAddress: 'bill2@microsoft.com', emailExtension: '@microsoft.com',
-                                                emailStatus: 'Active', dateCreated: new Date())
+                emailStatus: 'Active', dateCreated: new Date())
         email3.save(flush: true)
         bill2Email.add(email3)
 
         HashSet<EmailAddress> steve2Email = new HashSet<EmailAddress>()
         EmailAddress email4 = new EmailAddress(emailType: 'Primary', emailAddress: 'steve2@apple.com', emailExtension: '@apple.com',
-                                                emailStatus: 'Active', dateCreated: new Date())
+                emailStatus: 'Active', dateCreated: new Date())
         email4.save(flush: true)
         steve2Email.add(email4)
 
 
         // People, Companies
-        Company microsoft = new Company()
+        Company microsoft = new Company(companyName: 'Microsoft')
         microsoft.save(flush: true)
         Contact billGates = new Contact(salutation: 'Mr.', firstName: 'Bill', lastName: 'Gates', gender: 'M',
-                                        jobRole: 'Management', jobTitle: 'CEO', roleDescription: 'Description', department: 'Dept.',
-                                        company: microsoft, emailAddresses: billEmail)
+                jobRole: 'Management', jobTitle: 'CEO', roleDescription: 'Description', department: 'Dept.',
+                company: microsoft, emailAddresses: billEmail)
         Contact billGates2 = new Contact(salutation: 'Mr.', firstName: 'Bill2', lastName: 'Gates', gender: 'M',
-                                        jobRole: 'Management', jobTitle: 'CTO', roleDescription: 'Description', department: 'Dept.',
-                                        company: microsoft, emailAddresses: bill2Email)
+                jobRole: 'Management', jobTitle: 'CTO', roleDescription: 'Description', department: 'Dept.',
+                company: microsoft, emailAddresses: bill2Email)
         billGates.save(flush: true)
         billGates2.save(flush: true)
 
 
-        Company apple = new Company()
+        Company apple = new Company(companyName: 'Apple')
         apple.save(flush: true)
         Contact steveJobs = new Contact(salutation: 'Mr.', firstName: 'Steve', lastName: 'Jobs', gender: 'M',
                 jobRole: 'Management', jobTitle: 'CEO', roleDescription: 'Description', department: 'Dept.',
@@ -85,23 +91,20 @@ class BootStrap {
 
 
         // Leads
-        Lead lead1 = new Lead(status: '', leadStatus: 'Qualified', leadStage: 'MQL', budget: 'Yes',
-                                auth: 'Yes', need: 'Yes', timing: 'Yes', budgetDetails: 'budgetDetails1',
-                                authorityDetails: 'authDetails1', needDetails: 'needDetails1', timingDetails: new Date(),
-                                estimatedDealSize: 3450.15, dateOpened: new Date(), dateClosed: new Date(), contacts: lead1HashSet)
-
-        Lead lead2 = new Lead(status: '', leadStatus: 'Qualified', leadStage: 'MQL', budget: 'Yes',
-                                auth: 'Yes', need: 'Yes', timing: 'Yes', budgetDetails: 'budgetDetails2',
-                                authorityDetails: 'authDetails2', needDetails: 'needDetails2', timingDetails: new Date(),
-                                estimatedDealSize: 5867.35, dateOpened: new Date(), dateClosed: new Date(), contacts: lead2HashSet)
-
-
+        Lead lead1 = new Lead(status: 'Status 1', leadStatus: 'Qualified', leadStage: 'MQL', budget: 'Yes',
+                auth: 'Yes', need: 'Yes', timing: 'Yes', budgetDetails: 'budgetDetails1',
+                authorityDetails: 'authDetails1', needDetails: 'needDetails1', timingDetails: new Date(),
+                estimatedDealSize: 3450.15, dateOpened: new Date(), dateClosed: new Date(), contacts: lead1HashSet)
         lead1.save(flush: true)
+
+
+        Lead lead2 = new Lead(status: 'Status 2', leadStatus: 'Qualified', leadStage: 'MQL', budget: 'Yes',
+                auth: 'Yes', need: 'Yes', timing: 'Yes', budgetDetails: 'budgetDetails2',
+                authorityDetails: 'authDetails2', needDetails: 'needDetails2', timingDetails: new Date(),
+                estimatedDealSize: 5867.35, dateOpened: new Date(), dateClosed: new Date(), contacts: lead2HashSet)
         lead2.save(flush: true)
 
 
-    }
-    def destroy = {
     }
 
     def setupUsersAndRoles() {
@@ -109,7 +112,7 @@ class BootStrap {
                 bio: 'Some Bio', birthDate: new Date(), birthPlace: "Home Town", height: "6'2", weight: 160, universityAttended: "Some School", pictureURL: "https://pbs.twimg.com/media/Ce55dhKUYAAVEzF.jpg")
 
         saveObject(admin)
-        User user = new User(username: "deluongo", password:"Stephany",
+        User user = new User(username: "deluongo", password: "Stephany",
                 bio: 'Some Bio', birthDate: new Date(), birthPlace: "Home Town", height: "6'2", weight: 160, universityAttended: "Some School", pictureURL: "https://pbs.twimg.com/media/Ce55dhKUYAAVEzF.jpg")
         saveObject(user)
 
